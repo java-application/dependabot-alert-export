bcadfbc9c75e763e666636c626d595cea57b90c1
kimkm-mzc <[kimkm95@mz.co.kr](mailto:kimkm95@mz.co.kr)>
2023-09-11 00:23:01 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# dependabot-alert-export
+Export the Dependabot alerts as CSV file from a repo with filepath 
+
+This GitHub action helps to export the Dependabot alerts to a CSV file. One can define a workflow to run or triger based on specific event to capture all Dependabot alerts to a CSV file for further analysis. 
+
+
+## Release
+GitHub Marketplace : https://github.com/marketplace/actions/dependabot-alert-csv-export
+
+# How to Use the Action
+
+## PAT Token
+Create a PAT token to get access to the Dependabot alerts. Pass this token as an input to the action - GITHUB_TOKEN
+
+
+## action in workflow
+
+Include the dependabot-alert-export action in your workflow. 
+
+Following is the sample code for integrating this action with your workflow
+
+```
+steps:               
+      - name: Dependabot Alert CSV Export
+        uses: ShamKarthikS-Hexaware/dependabot-alert-export@v1.0
+        with:        
+          GITHUB_TOKEN: ${{secrets.GH_TOKEN}}
+          org_name: 'ORG_NAME'
+          repo_name: 'REPO_NAME'
+          csv_path: data/vulnerability.csv
+          
+      - name: Upload Vulnerability report
+        uses: actions/upload-artifact@v3
+        with:
+           name: vulnerability_report
+           path: data/vulnerability.csv          
+```
+
+## Parameters
+
+| Name                           | Required  | Description                                                                      |
+|--------------------------------|------------|----------------------------------------------------------------------|
+| GITHUB_TOKEN                 | Yes | PAT Token for access    |
+| org_name                       | Yes | GitHub Organization Name                                      |
+| repo_name                   | Yes | GitHub Repository Name     |
+| csv_path                       | Yes | CSV file path                                   |
+
+## Exported Fields
+Following fields are included in the Vulnerability Report
+- Vulnerability Id
+- Dependency Scope (`DEVELOPMENT` or `RUNTIME`)
+- State (`DISMISSED`, `FIXED` or `OPEN`)
+- Created At
+- Manifest File Name
+- Vulnerability Version Range
+- Package Name
+- GHSA Id (The ID of the vulnerability in the [GitHub Security Advisory Database](https://github.com/advisories))
+- Severity
+- Summary
+- Link
+- Description
+- Dismissed At (for Dismissed alerts)
+- Dismiss Reason
+- Dismiss Comment
+- Fixed At (for Fixed alerts)
+- Fix Reason
+
+## Report
+Vulnerability report in CSV format will be available as part of the build artifacts for download
+
+<img width="792" alt="Screenshot 2022-09-18 at 1 23 26 PM" src="https://user-images.githubusercontent.com/10282550/190891852-13c25b39-3779-4754-a2e5-7f431b2807c4.png">
+
+# License
+
+The scripts and documentation in this project are released under the [MIT License](https://github.com/actions/download-artifact/blob/main/LICENSE)
+
+
---
+++ app/index.js
@@ -0,0 +1,212 @@
+// libs for github & graphql
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { parse } = require('json2csv');
+
+// libs for csv file creation
+const { dirname } = require("path");
+const makeDir = require("make-dir");
+
+// get the octokit handle 
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+// inputs defined in action metadata file
+const org_Name = core.getInput('org_name');
+const repo_Name = core.getInput('repo_name');
+// const csv_path = core.getInput('csv_path') + new Date().toISOString();
+
+// Graphql query for vulnerability data
+const query =
+  `query ($org_name: String! $repo_name: String! $pagination: String){
+      repository(owner: $org_name name: $repo_name) {
+        name
+        vulnerabilityAlerts(first: 50 after: $pagination) {     
+          pageInfo {
+              hasNextPage
+              endCursor
+          }    
+          totalCount
+          nodes {
+            id
+            createdAt
+            state
+            dismissedAt
+            dismissReason
+            dismissComment
+            fixedAt
+            fixReason
+            dependencyScope
+            repository{
+              name
+              owner{
+                login
+              }
+            }
+            securityAdvisory {
+                ghsaId
+                description
+                permalink
+                severity
+                summary
+            }
+            securityVulnerability {
+              package {
+                name
+              }
+              vulnerableVersionRange
+            }
+            vulnerableManifestFilename
+            vulnerableManifestPath
+          }
+        }
+      }
+    }`
+
+  // Our CSV output fields
+  const fields = [{
+    label: 'Owner',
+    value: 'repository.owner.login',
+    default: `${org_Name}`
+  },
+  {
+    label: 'Repository Name',
+    value: 'repository.name',
+    default: `${repo_Name}`
+  },
+  {
+    label: 'ID',
+    value: 'id'
+  },
+  {
+    label: 'State',
+    value: 'state'
+  },
+  {
+    label: 'Scope',
+    value: 'dependencyScope'
+  },
+  {
+    label: 'Created At',
+    value: 'createdAt'
+  },
+  {
+    label: 'Vulnerability Version Range',
+    value: 'securityVulnerability.vulnerableVersionRange'
+  },
+  {
+    label: 'Package Name',
+    value: 'securityVulnerability.package.name'
+  },
+  {
+    label: 'GHSA Id',
+    value: 'securityAdvisory.ghsaId'
+  },
+  {
+    label: 'Severity',
+    value: 'securityAdvisory.severity'
+  },
+  {
+    label: 'Summary',
+    value: 'securityAdvisory.summary'
+  },
+  {
+    label: 'Link',
+    value: 'securityAdvisory.permalink'
+  },
+  {
+    label: 'Description',
+    value: 'securityAdvisory.description'
+  },
+  {
+    label: 'Dismissed At',
+    value: 'dismissedAt'
+  },
+  {
+    label: 'Dismiss Reason',
+    value: 'dismissReason'
+  },
+  {
+    label: 'Dismiss Comment',
+    value: 'dismissComment'
+  },
+  {
+    label: 'Fixed At',
+    value: 'fixedAt'
+  },
+  {
+    label: 'Fix Reason',
+    value: 'fixReason'
+  },
+  {
+    label: 'Manifest File Path',
+    value: 'vulnerableManifestPath'
+  },
+  {
+    label: 'Manifest File Name',
+    value: 'vulnerableManifestFilename'
+  },
+  ];
+
+// graphql query execution
+async function getAlerts(org, repo, pagination) {
+  try {
+    console.log(`getAlerts(): pagination: ${pagination ? pagination: null}` );
+
+    return await octokit.graphql(query, { org_name: `${org}`, repo_name: `${repo}`, pagination: (pagination ? `${pagination}` : null) });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+// Extract vulnerability alerts with a pagination of 50 alerts per page
+async function run(org_Name, repo_Name, csv_path) {
+
+  let pagination = null;
+  let hasNextPage = false;
+  let addTitleRow = true;
+  let alertCount =0 ;
+
+  try {
+    await makeDir(dirname(csv_path));
+    do {
+      // invoke the graphql query execution
+      await getAlerts(org_Name, repo_Name, pagination).then(alertResult => {
+        let vulnerabilityNodes = alertResult.repository.vulnerabilityAlerts.nodes;
+        
+        if(addTitleRow){
+          alertCount = alertResult.repository.vulnerabilityAlerts.totalCount;
+          console.log ('Alert Count ' + alertCount);
+        }
+        
+        // ALERT! - create our updated opts
+        const opts = { fields, "header": addTitleRow };
+  
+        // append to the existing file (or create and append if needed)
+        require("fs").writeFileSync(csv_path, `${parse(vulnerabilityNodes, opts)}\n`);
+
+        // pagination to get next page data
+        let pageInfo = alertResult.repository.vulnerabilityAlerts.pageInfo;
+        hasNextPage = pageInfo.hasNextPage;
+        if (hasNextPage) {
+          pagination = pageInfo.endCursor;
+          addTitleRow = false;
+        }
+        console.log(`run(): hasNextPage:  ${hasNextPage}`);
+        console.log(`run(): Pagination cursor: ${pagination}`);
+        console.log(`run(): addTitleRow: ${addTitleRow}`);
+
+      });
+      
+       core.setOutput('alerts_count', alertCount)
+
+    } while (hasNextPage);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+console.log(`preamble: org name: ${org_Name}   repo name: ${repo_Name} csv_path: ${csv_path}` );
+
+// run the action code
+run(org_Name, repo_Name, csv_path);
